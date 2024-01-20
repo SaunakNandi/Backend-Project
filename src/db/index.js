5c3c08893887ecdf62b4fda3e6ba1ad17cb495b8
Saunak Nandi <saunaknandi021000@gmail.com>
2024-01-20 19:03:58 +0530

    connect to database

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants.js"
+
+const connectDB=async()=>{
+    try {
+        // catching responses after connection is done
+        const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`\n mongodb connected !! db host:
+        ${connectionInstance.connection.host}`)  // connection.host tell us in which database we are connected (dev,prod or testing)
+    } catch (error) {
+        console.log("Mongodb connection error",error)
+
+        // the current application is running on a process and this process is a reference of it which comes
+        //  from nodejs
+        process.exit(1) // [node process failure exit code 1]
+    }
+}
+export  default connectDB
