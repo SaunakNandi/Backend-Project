4e9e8d1cf2a75895a2eb4135cf21705a63314cd7
Saunak Nandi <saunaknandi021000@gmail.com>
2024-01-26 10:02:58 +0530

    cloudinary and multer setup

---
+++ src/middlwwares/multer.middleware.js
@@ -0,0 +1,15 @@
+import multer from "multer";
+
+// file uploading
+const storage = multer.diskStorage({
+  // cb is the callback function
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ storage: storage });
