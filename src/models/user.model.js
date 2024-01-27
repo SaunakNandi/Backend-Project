e974e6571bfd2798c1517b8137b35a9e26b0a11e
Saunak Nandi <saunaknandi021000@gmail.com>
2024-01-27 07:25:19 +0530

    register controllers

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next(); // if not modified don't update the password
 
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
