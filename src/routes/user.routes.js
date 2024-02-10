9fc4fe89cc340f2e617faf71d7c59d18d7336ad7
Saunak Nandi <saunaknandi021000@gmail.com>
2024-02-10 15:32:19 +0530

    changes in router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,7 +3,12 @@ import {
   loginUser,
   logoutUser,
   registerUser,
-  refreshAccessToken
+  refreshAccessToken,
+  getCurrentUser,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -34,5 +39,14 @@ router.route("/logout").post(verifyJWT, logoutUser);
 // discussed in the part2 video tutorial at 1:11:25
 // endpoint for refresh tokens
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account").patch(verifyJWT,updateAccountAccount);   // not using post as it will update all the details
+
+//upload.single("avatar") is the middleware
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);  // using post wll upfate all 
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+router.route.apply("/history").get(verifyJWT,getWatchHistory);
 
 export default router;
